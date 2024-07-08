6df0c8413f6beb8412e8619da1dd6b0b0223d4fa
FranzKieviet <franzkieviet@gmail.com>
2024-07-07 23:30:31 -0700

    Pipeline connected

--- buildData.py
+++ buildData.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv, dotenv_values
 import os
 import cv2
 import numpy as np
+import boto3
+import uuid
+import json
 
 ###         CONSTANTS:      ###
 
@@ -10,15 +13,28 @@ import numpy as np
 TOP_REALITORS = ["amazon", "ebay", "walmart", "target", "zappos", "newegg"]
 params = {
     "engine": "google_reverse_image", 
-    "api_key": os.getenv("my_api_key")
+    "api_key": ""
 }
+
+session = boto3.Session(
+    aws_access_key_id='',
+    aws_secret_access_key='',
+    region_name='us-west-1'
+)
+
+s3 = session.client('s3')
+bucket = 'image-bucket-for-tiktok'
 
 load_dotenv()
 
 #Collage -> Images:
-def getImages(path = '/collage2.png'):
+def getImages(path = 'collages/', collage = 'collage'):
+    num = 0
+    images = []
+    uuids = []
+
     # Read the image
-    image = cv2.imread(path)
+    image = cv2.imread(path+collage+".png")
     if image is None:
         print("Error: Could not load the image. Check the file path.")
 
@@ -31,7 +47,6 @@ def getImages(path = '/collage2.png'):
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Loop over contours and extract images
-    num = 0
     buffer = 10
     for i, contour in enumerate(contours):
         x, y, w, h = cv2.boundingRect(contour)
@@ -43,20 +58,47 @@ def getImages(path = '/collage2.png'):
         
         if w >= min_width and h >= min_height:
             num += 1
-            cv2.imwrite(f'/home/image_{i}.jpg', extracted_image) #TODO FIX THIS
+            new_uuid = str(uuid.uuid4())
+            location = f'outputs/{new_uuid}.jpg'
+            cv2.imwrite(location, extracted_image)
+            uuids.append(new_uuid)
+            images.append(location)
+
     print(num, " objects found")
+    return images, uuids
+
+#list of images holds path and image [a.png, b.png...]
+def upload_images(images, uuids, bucket=bucket): 
+    counter = 0
+    for file in images: 
+        key_name = uuids[counter]+".jpg"
+        s3.upload_file(f'{file}', bucket, key_name, ExtraArgs={'ContentType': 'jpg'})
+
+        counter += 1 
+
+
+def get_images(uuids, bucket=bucket):
+    urls = []
+    for uuid in uuids:
+        url = f"https://{bucket}.s3.amazonaws.com/{uuid}.jpg"
+        urls.append(url)
+    return urls
 
 #Image -> URL:
-def getURLS(images):
-    images = ["https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1130px-Red_Apple.jpg"]
+
+def getURLS(images, uuids, contexts): #Images is the public urls
     top_results = []
     other_results = []
     toReturn = {}
+    data = []
+    counter = 0
+    contexts = ["blah"]
 
     #Call API and search of images
     for image in images: 
         params["image_url"] = images[0]
         search = GoogleSearch(params) 
+        #print(search.get_dict())
         results = search.get_dict()["image_results"]
 
         for result in results: 
@@ -71,7 +113,35 @@ def getURLS(images):
             if (not isTopReal):       
                 other_results.append(link)
         toReturn[image] = {"top_results": top_results, "other_results": other_results}
-        print(top_results)
-        print(other_results)
 
-    return toReturn
+
+        #Create object to store the data:
+        obj = {
+            "uuid": uuids[counter],
+            "image_link": image,
+            "top_results": top_results,
+            "other_results": other_results,
+            "context": contexts
+        }
+
+        data.append(obj)
+        counter += 1
+    return data
+
+def main():
+    collages = ['collage']# , 'collage2']
+
+    for c in collages:
+        images, uuids = getImages(path = 'collages/', collage = c)
+        upload_images(images, uuids)
+        imageURLS = get_images(uuids)
+        #print(imageURLS)
+        data = getURLS(imageURLS, uuids, [])
+
+        file_path = "data.json"
+        with open(file_path, 'w') as f:
+            json.dump(data, f, indent=2)
+
+        print(f"JSON data saved to {file_path}")
+
+main()
